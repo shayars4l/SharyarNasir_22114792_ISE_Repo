f70f678c56d08e6c2ae4bf418148716846071840
sharyar <22114792@student.curtin.edu.au>
2025-11-23 22:48:11 +0400

    main file for exicutions of code

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+"""
+Entry point for ICSC program.
+Usage: python3 main.py
+"""
+
+from loader import load_services
+from ui import main_menu
+
+def run():
+    try:
+        services = load_services('services.csv')
+    except Exception as e:
+        print(f"Failed to load services.csv: {e}")
+        return
+    main_menu(services)
+
+if __name__ == "__main__":
+    run()
